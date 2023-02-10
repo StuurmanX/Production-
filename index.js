7bf83fa585412cdc093ef7d5309972559248f6b2
Xolile <117374793+StuurmanX@users.noreply.github.com>
2023-02-10 18:20:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ for (let i = 1; i <= 3; i++) {
       window.location.href = `/row-${i}`;
     });
   }
+
+  let counter = document.getElementById("counter");
+  let status = document.getElementById("status");
+  let interval;
+
+  status.addEventListener("change", function() {
+    clearInterval(interval);
+    counter.innerHTML = 0;
+    if (status.value === "Minor clean") {
+      interval = setInterval(function() {
+        counter.innerHTML = parseInt(counter.innerHTML) + 1;
+      }, 1000);
+    }
+   });
